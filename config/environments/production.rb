ab12030b67e28ff35cacb82413c95137141275df
Raymond Aranda <raymond.aranda@gmail.com>
2018-03-08 15:19:53 -0600

    host fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'cp-lunch-roulette.herokuapp.com'}
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {domain: 'cp-lunch-roulette.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://app.mailgun.com/app/domains/sandbox18de7726c70e497eb061a4350f3b00b8.mailgun.org'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
